1d1d1c009c649c208f00b3b4bb12a1a8a7c78569
unknown <amit.ramesh.sharma@gmail.com>
2026-03-16 19:42:28 +0530

    User Sign up functionality

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoute from './routes/auth.route.js';
-import connectDB from './db/db.js';
-
+import connectDB from './lib/db/db.js'
 
 dotenv.config();
 const app = express();
-connectDB();
+app.use(express.json());
 
 app.get('/',(req,res)=>{
     res.send("Welcome to Chat Application")
@@ -15,6 +14,7 @@ app.get('/',(req,res)=>{
 app.use("/api/auth/",authRoute);
 
 app.listen(3000,()=>{
+    connectDB();
     console.log("Server is running PORT ",process.env.PORT);
 });
 
