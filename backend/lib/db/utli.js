39539459e660d996c84a5321ec26847111a8e37f
unknown <amit.ramesh.sharma@gmail.com>
2026-03-17 13:05:01 +0530

    User login functionality

--- backend/lib/db/utli.js
+++ backend/lib/db/utli.js
@@ -5,7 +5,7 @@ export const generateToken = async (userId, res)=>{
 const token = jwt.sign(userId,process.env.JWT_SECRET);
 console.log("Token: ", token)
 res.cookie("jwt",token,{
-    maxAge:7*24*60*60*1000, //MS
+    maxAge:7 * 24 * 60 * 60 * 1000, //MS
     httpOnly:true, //prevent XSS cross site scripting
     sameSite:"strict", //CSRF attacks cross-site request forgery attacks
     secure:process.env.NODE_ENV !== "development",
